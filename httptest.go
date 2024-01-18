5980e6c1abf618e83d06c6e9b6765c8cbc863ec1
TerryW-tx <58629199+TerryW-tx@users.noreply.github.com>
2024-01-18 17:45:38 +0800

    rename conflict variables, complete post function

--- httptest.go
+++ httptest.go
@@ -2,22 +2,28 @@ package httptest
 
 import (
     "net/http"
-	"strings"
+	"net/url"
+	// "strings"
     // "net/http/httptest"
     // "testing"
 )
 
 type Test struct {
-	host string
-	port string
-	url string
+	caseName string
+	// host string
+	// port string
+	gUrl string
 	result *http.Response
+	body map[string][]string
 }
 
-func (t *Test) Get(url string) {
-	t.result, _ = http.Get(url)
+func (t *Test) Get(URL string) {
+	t.result, _ = http.Get(URL)
+	// println(URL)
+	// println(t.result)
 }
 
-func (t *Test) Post(url string) {
-	t.result, _ = http.Post(url, "application/json", strings.NewReader("name=cjb"))
+func (t *Test) Post(URL string, body map[string][]string) {
+	payload := url.Values(body)
+	t.result, _ = http.PostForm(URL, payload)
 }
